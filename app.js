a8430d5347b46328d0470b9c8c4390a9b5afe846
nigarnuraliyeva0226 <nikanurali0226@gmail.com>
2023-08-27 16:18:33 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let d = new Date ();
+document.getElementById ("date").innerHTML=d;
+console.log(d);
+
+let z = new Date (2004,12,11);
+document.getElementById ("date_").innerHTML=z;
+console.log(z);
+
+let  m = "Indiye qeder neche gun yashamisiz?";
+console.log(m);
+
+let n="Cavab";
+console.log(n);
+
+console.log(Math.ceil((d-z)/86400000));
